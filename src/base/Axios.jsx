8cf4651083bfca0479377b2e2b0a223794d53e7d
Abdellah <abdellahmoiuda@gmail.com>
2024-03-10 10:11:05 +0000

    Netlify Confing added

--- src/base/Axios.jsx
+++ src/base/Axios.jsx
@@ -1,6 +1,19 @@
 import axios from "axios";
 
 let Axios = axios.create({
-  baseURL: "https://hiworld-server.onrender.com/",
+  baseURL: "https://hiworld-server.onrender.com",
 });
+// Add a request interceptor
+Axios.interceptors.request.use(
+  function (config) {
+    // Modify config to include CORS headers
+    config.headers["Access-Control-Allow-Origin"] =
+      "https://hiworld-client.onrender.com";
+    return config;
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
 export default Axios;
