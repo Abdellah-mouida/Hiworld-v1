9d105938a7c0ca7d52dd5f4109bcc65ebcc588dc
Abdellah <abdellahmoiuda@gmail.com>
2024-03-10 10:52:11 +0000

    ...

--- src/base/Axios.jsx
+++ src/base/Axios.jsx
@@ -1,19 +1,7 @@
 import axios from "axios";
 
 let Axios = axios.create({
-  baseURL: "https://hiworld-server.onrender.com",
+  baseURL: "https://hiworld-server.onrender.com/",
 });
-// Add a request interceptor
-Axios.interceptors.request.use(
-  function (config) {
-    // Modify config to include CORS headers
-    config.headers["Access-Control-Allow-Origin"] =
-      "https://hiworld-client.netlify.app";
-    return config;
-  },
-  function (error) {
-    // Do something with request error
-    return Promise.reject(error);
-  }
-);
+
 export default Axios;
